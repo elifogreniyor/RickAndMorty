87d419ccf0f2faeb521e48a8b2909b65d79e0c8c
elifogreniyor <elfogreniyor@gmail.com>
2023-11-27 13:38:18 +0300

    make folder structure + tab view controller

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Elif Pamuk on 27.11.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Elif Pamuk on 27.11.2023.
+//
+
+import Foundation
